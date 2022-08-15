9394438448b2cdbca747b958d3ae39c4ea7fdc66
rite2rio <marionoronha.s@gmail.com>
2022-08-15 19:01:29 +0530

    few changes to the code file

--- model.py
+++ model.py
@@ -30,7 +30,7 @@ class SentimentRecommenderModel:
             SentimentRecommenderModel.ROOT_PATH + SentimentRecommenderModel.VECTORIZER)
         self.user_final_rating = pickle.load(open(
             SentimentRecommenderModel.ROOT_PATH + SentimentRecommenderModel.RECOMMENDER, 'rb'))
-        self.data = pd.read_csv("dataset/sample30.csv")
+        self.data = pd.read_csv("dataset/ProductReviews.csv")
         self.cleaned_data = pickle.load(open(
             SentimentRecommenderModel.ROOT_PATH + SentimentRecommenderModel.CLEANED_DATA, 'rb'))
         self.lemmatizer = WordNetLemmatizer()
